f0f66e0c6a8674264b3004df4b1a3578ec534e19
Pedro Carrazco <fzn.webdesign@gmail.com>
2014-02-10 16:18:48 -0600

    Gravity added

--- js/fznSprites.js
+++ js/fznSprites.js
@@ -2,12 +2,13 @@ var fzn = fzn || {};
 fzn.Sprite = function (game,params){
 	if(game instanceof fzn.Game){
 		this.game = game;
-		this.canvas = null;
-		this.cnv = null;
 		this.size = [];
+		this.velDown = 0;
+		this.gravity = 1.19;
 		this.action = "stand";
 		this.frame = 0;
 		this.params = params;
+		this.pos = [];
 		this.id = "fznSP_"+Math.round(Math.random()*100000);
 		this.init();
 	}else{
@@ -20,13 +21,18 @@ fzn.Sprite.prototype = {
 		this.params.step = this.params.step || 2;
 		this.params.actions = this.params.actions || [];
 		this.action = this.params.action || this.action;
+		this.pos = this.params.pos || [0,0];
 		
 		// Generate a canvas for Sprite
-		this.setRes();
 		this.loadSheet(this.params.source);
 	},
 	go: function(){
 		var frame = this.frame;
+		if(this.gravity>0){
+			this.velDown = this.velDown + this.gravity;
+			this.pos[1] = this.pos[1]+this.velDown;
+			this.pos[1] = (this.pos[1] > (this.game.cnv.height - this.size[1])) ? this.game.cnv.height - this.size[1] : this.pos[1];
+		}
 		this.active = this.params.actions[this.action];
 		if(this.active.turn != 0 && this.game.turn % this.active.turn == 0){
 			frame++;
@@ -41,43 +47,22 @@ fzn.Sprite.prototype = {
 		}
 	},
 	redraw: function(){
-		this.canvas.clearRect(0,0,this.cnv.width,this.cnv.height);
-		this.canvas.drawImage(
+		this.game.canvas.drawImage(
 			this.sheet,
 			this.active.steps[this.frame][0],
 			this.active.steps[this.frame][1],
 			this.size[0],
 			this.size[1],
-			0,
-			0,
+			this.pos[0],
+			this.pos[1],
 			this.size[0],
 			this.size[1]
 		);
 	},
-	setRes: function(){
-		var i,len,theID,
-			found=false;
-		this.res = this.game.getRes();
-		for(i=0,len=this.res.childNodes.length;i<len;i++){
-			theID = this.res.childNodes[i].id || false;
-			if(theID && theID = this.id){
-				this.cnv = this.res.childNodes[i];
-				found = true;
-			}
-		}
-		if(!found){
-			this.size = this.params.size;
-			this.cnv = document.createElement("canvas");
-			this.cnv.id = this.id;
-			this.cnv.width = this.size[0];
-			this.cnv.height = this.size[1];
-			this.res.appendChild(this.cnv);
-		}
-		this.canvas = this.cnv.getContext('2d')
-	},
 	loadSheet: function(source){
 		var src = source || false,
 			self = this;
+		this.size = this.params.size;
 		self.sheet = new Image()
 		self.sheet.addEventListener("load", function() {
 			self.game.loadQueue--;
