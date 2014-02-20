db0e15b4fc75e687886cd3ac4833d47f7804f016
Pedro Carrazco <fzn.webdesign@gmail.com>
2014-02-20 15:43:44 -0600

    Update fznBackgrounds.js

--- js/fznBackgrounds.js
+++ js/fznBackgrounds.js
@@ -5,8 +5,14 @@ fzn.Background = function (game,params){
 		this.game = game;
 		this.image = null;
 		this.data = params.data || {};
-		this.size = params.size || [10,10];;
-		this.pos = params.pos || [0,0];;
+		this.size = params.size || [10,10];
+		this.pos = params.pos || [0,0];
+		this.velDown = 0;
+		this.gravity = 1;
+		this.maxVel = 11;
+		this.apos = [0,0];
+		this.animation = params.animation || false;
+		this.animationEnd = false;
 		this.opacity = (typeof params.opacity != "undefined") ? params.opacity : 1;
 		this.source = params.source || false;
 		this.repeat = params.repeat || false;
@@ -20,31 +26,67 @@ fzn.Background = function (game,params){
 fzn.Background.prototype = {
 	init: function(){
 		// Generate a canvas for BG
+		if(this.pos == "center"){
+			this.pos = []
+			this.pos[0] = (this.game.cnv.width / 2) - (this.size[0] / 2);
+			this.pos[1] = (this.game.cnv.height / 2) - (this.size[1] / 2);
+			console.log(this.pos)
+		}
+		if(this.animation){
+			switch(this.animation){
+				case "bounce":
+					this.apos = [this.pos[0],0-this.size[1]];
+				break;
+			}
+		}
 		this.loadImage(this.source);
 	},
 	go: function(){
-		var newposx = this.pos[0],
-			newposy = this.pos[1];
-		
-		this.pos = [newposx,newposy]
+		if(this.animation && !this.animationEnd){
+			switch(this.animation){
+				case "fall":
+					if(this.apos[1] < this.pos[1]){
+						this.velDown += this.gravity;
+						this.velDown = (this.velDown > this.maxVel) ? this.maxVel : this.velDown;
+						this.apos[1] += this.velDown;
+					}else{
+						this.apos[1] == this.pos[1];
+						this.animationEnd = true
+					}
+				break;
+				case "bounce":
+					this.velDown += this.gravity;
+					this.apos[1] += this.velDown;
+					if(this.apos[1] > this.pos[1]){
+							this.apos[1] = this.pos[1]
+						if(this.velDown<0.5){
+							this.animationEnd = true
+						}else{
+							this.velDown = -(this.velDown/2);
+						}
+					}
+				break;
+			}
+		}
 		this.redraw();
 	},
 	redraw: function(){
-		var x,y,sX,sY;
+		var x,y,sX,sY,
+			pos=(this.animation && !this.animationEnd) ? this.apos : this.pos;
 		this.game.canvas.save();
 		if(this.fixed === true){
-			x = this.pos[0];
-			y = this.pos[1];
-			sX = this.pos[0];
-			sY = this.pos[0];
+			x = pos[0];
+			y = pos[1];
+			sX = pos[0];
+			sY = pos[0];
 		}else if(this.fixed === false){
-			x = this.pos[0] - this.game.level.pos[0]; 
-			y = this.pos[1] - this.game.level.pos[1];
+			y = pos[1] - this.game.level.pos[1];
+			x = pos[0] - this.game.level.pos[0]; 
 			sX = this.game.level.pos[0];
 			sY = this.game.level.pos[1];
 		}else{
-			x = this.pos[0] - (this.game.level.pos[0]*this.fixed); 
-			y = this.pos[1] - (this.game.level.pos[1]*this.fixed);
+			x = pos[0] - (this.game.level.pos[0]*this.fixed); 
+			y = pos[1] - (this.game.level.pos[1]*this.fixed);
 			sX = this.game.level.pos[0]*this.fixed;
 			sY = this.game.level.pos[1]*this.fixed;
 		}
