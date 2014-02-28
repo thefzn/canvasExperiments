1d339d98fb1748471d42374e2ba249c72783db7c
Pedro Carrazco <fzn.webdesign@gmail.com>
2014-02-28 16:04:54 -0600

    Update fznAnimations.js

--- js/fznAnimations.js
+++ js/fznAnimations.js
@@ -5,61 +5,185 @@ fzn.Animation = function (item,anim,params){
 	params = params || {};
 	this.item = item;
 	this.game = item.game;
-	this.size = params.size || [50,20];
-	this.pos = item.pos || [0,0];
-	this.apos = params.apos || [0,0];
+	this.size = params.size || this.item.size || [50,50];
+	this.endPosition = item.pos || [0,0];
+	this.pos = params.apos || [0,0];
 	this.velDown = params.velDown || 0;
 	this.gravity = params.gravity || 1;
 	this.maxVel = params.maxVel || 11;
 	this.anim = anim || false;
-	this.opacity = (typeof params.opacity != "undefined") ? params.opacity : 1;
+	this.animationEnd = true;
+	this.endOpacity = (typeof params.opacity != "undefined") ? params.opacity : (typeof this.item.opacity != "undefined") ? this.item.opacity : 1;
+	this.opacity = (typeof params.aOpacity != "undefined") ? params.aOpacity : 0;
 	this.loop = params.loop || false;
 	this.id = params.id;
+	this.callback = (typeof params.callback == "function") ? callback : this.callback;
 	this.init();
 }
 fzn.Animation.prototype = {
 	init: function(){
+		this.pos = this.checkCenter(this.pos);
+		if(this.anim){
+			this.start();
+		}
+	},
+	start:function(theAnim,params,callback){
+		var params = params || false,
+			parent = this.item.parent || this.item.menu || this.item.level || this.item.game,
+			temp;
+		this.anim = theAnim || this.anim || false;
+		if(!this.anim || !this.animationEnd){
+			return false;
+		}
+		this.callback = (typeof callback == "function") ? callback : this.callback;
+		this.endOpacity = (typeof this.endOpacity != "undefined") ? this.endOpacity : 1;
+		this.endPosition = this.endPosition || [0,0];
 		switch(this.anim){
 			case "bounce":
-				this.apos = [this.pos[0],0-this.size[1]];
+				temp = (typeof parent.pos != "undefined") ? parent.pos[1] : 0;
+				this.pos = [this.endPosition[0],temp-this.size[1]];
+				this.item.pos = this.pos;
+			break;
+			case "fallIn":
+				this.pos = [this.endPosition[0],this.endPosition[1]-this.size[1]];
+				this.anim = "fall";
+				this.item.pos = this.pos;
+			break;
+			case "fallOut":
+				this.endPosition = [this.item.pos[0], this.item.pos[1] + this.item.game.cnv.height];
+				this.pos = [this.item.pos[0], this.item.pos[1]];
+				this.anim = "fall";
+				this.item.pos = this.pos;
+			break;
+			case "fadeIn":
+				this.opacity = 0;
+			break;
+			case "fadeOut":
+				this.opacity = this.endOpacity;
+			break;
+			case "move":
+				if(params && params instanceof Array){
+					this.pos = [this.item.pos[0], this.item.pos[1]];
+					this.endPosition = this.checkCenter(params);
+				}else{
+					return false;
+				}
 			break;
 			default:
-				this.apos = this.pos;
+				this.pos = [this.endPosition[0],this.endPosition[1]];
 		}
-		this.item.pos = this.apos;
+		this.animationEnd = false;
+	},
+	stop: function(){
+		this.opacity = this.endOpacity;
+		this.pos = [this.endPosition[0],this.endPosition[1]];
+		this.item.pos = this.pos;
+		this.item.opacity = this.opacity;
+		this.animationEnd = true;
+		this.onEnd();
+	},
+	checkCenter: function(pos){
+		var w,h,
+			parent = this.item.parent || this.item.menu || this.item.level || this.item.game;
+		if(pos == "center"){
+			pos = [];
+			pos[0] = (this.item.game.cnv.width / 2) - (this.item.size[0] / 2);
+			pos[1] = (this.item.game.cnv.height / 2) - (this.item.size[1] / 2);
+		}else{
+			w = (typeof parent.size != "undefined") ? parent.size[0] : parent.cnv.width;
+			h = (typeof parent.size != "undefined") ? parent.size[1] : parent.cnv.height;
+			pos[0] = (pos[0] == "center") ? (w / 2) - (this.item.size[0] / 2) : pos[0];
+			pos[1] = (pos[1] == "center") ? (h / 2) - (this.item.size[1] / 2) : pos[1];
+		}
+		return pos;
+	},
+	callback: function(self,anim){
+	
 	},
 	go: function(){
 		var item,target;
 		target = this.anims[this.anim];
 		if(typeof target == "function" && !this.animationEnd){
-				target.call(this);
+			target.call(this);
 		}
 	},
 	anims: {
 		fall: function(){
-			if(this.apos[1] < this.pos[1]){
+			if(this.pos[1] < this.endPosition[1]){
 				this.velDown += this.gravity;
 				this.velDown = (this.velDown > this.maxVel) ? this.maxVel : this.velDown;
-				this.apos[1] += this.velDown;
-				this.item.pos = this.apos
-			}else{
+				this.pos[1] += this.velDown;
+				this.pos[1] = (this.pos[1] > this.endPosition[1]) ? this.endPosition[1] : this.pos[1];
 				this.item.pos = this.pos
-				this.animationEnd = true
+			}else{
+				this.item.pos = this.endPosition;
+				this.onEnd();
 			}
 		},
 		bounce: function(){
 			this.velDown += this.gravity;
-			this.apos[1] += this.velDown;
-			if(this.apos[1] > this.pos[1]){
-				this.apos[1] = this.pos[1]
-				this.item.pos = this.apos;	
-				if(this.velDown<0.5){
-					this.item.pos = this.pos;
-					this.animationEnd = true
+			this.pos[1] += this.velDown;
+			if(this.pos[1] > this.endPosition[1]){
+				this.pos[1] = this.endPosition[1]
+				this.item.pos = this.pos;
+				if(this.velDown < 1){
+					this.item.pos = this.endPosition;
+					this.onEnd();
 				}else{
 					this.velDown = -(this.velDown/2);
 				}
 			}	
-		}			
+		},
+		jump: function(){
+			this.velDown += this.gravity;
+			this.pos[1] += this.velDown;
+			if(this.pos[1] >= this.endPosition[1]){
+				this.velDown = 0-this.maxVel;
+			}
+			this.item.pos = this.pos;
+		},
+		fadeIn: function(){
+			this.opacity += 0.05;
+			if(this.opacity > this.endOpacity){
+				this.opacity = this.endOpacity;
+				this.onEnd();
+			}
+			this.item.opacity = this.opacity;
+		},
+		fadeOut: function(){
+			this.opacity -= 0.05;
+			if(this.opacity < 0){
+				this.opacity = 0;
+				this.onEnd();
+			}
+			this.item.opacity = this.opacity;
+		},
+		move: function(){
+			var step = 7,
+				rX = false,
+				rY = false;
+			this.pos[0] += (this.endPosition[0] - this.pos[0]) / step;
+			this.pos[1] += (this.endPosition[1] - this.pos[1]) / step;
+			
+			if(Math.abs(this.endPosition[0] - this.pos[0]) < 1){
+				this.pos[0] = this.endPosition[0];
+				rX = true;
+			}
+			if(Math.abs(this.endPosition[1] - this.pos[1]) < 1){
+				this.pos[1] = this.endPosition[1];
+				rY = true;
+			}
+			this.item.pos = this.pos;
+			if(rX && rY){
+				this.onEnd();
+			}
+			
+		}
+	},
+	onEnd: function(){
+		var cB = this.callback;
+		this.animationEnd = true;
+		this.callback = function(){};
+		cB(this.item,this);
 	}
 }
