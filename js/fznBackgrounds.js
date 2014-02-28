eae0d3379a8e0cb55f3cd377df5208401247a7ef
Pedro Carrazco <fzn.webdesign@gmail.com>
2014-02-28 16:04:37 -0600

    Update fznBackgrounds.js

--- js/fznBackgrounds.js
+++ js/fznBackgrounds.js
@@ -3,17 +3,18 @@ fzn.Background = function (game,params){
 	if(game instanceof fzn.Game){
 		// Data Vars
 		this.game = game;
+		this.menu = params.menu || false;
 		this.data = params.data || {};
-		this.size = params.size || [10,10];
+		this.parent = params.menu || game.level || false;
+		this.size = params.size || [this.game.cnv.width,this.game.cnv.height];
 		this.pos = params.pos || [0,0];
-		this.animation = params.animation || false;
-		this.anim = null;
-		this.animationEnd = false;
+		this.color = params.color || "transparent";
 		this.opacity = (typeof params.opacity != "undefined") ? params.opacity : 1;
 		this.source = params.source || false;
 		this.repeat = params.repeat || false;
 		this.fixed = params.fixed || false;
 		this.id = params.id;
+		this.animation = params.animation || false;
 		this.init();
 	}else{
 		return false;
@@ -21,18 +22,22 @@ fzn.Background = function (game,params){
 }
 fzn.Background.prototype = {
 	init: function(){
-		// Generate a canvas for BG
-		this.pos[0] = (this.pos[0] == "center") ? (game.cnv.width / 2) - (this.size[0] / 2) : this.pos[0];
-		this.pos[1] = (this.pos[1] == "center") ? (game.cnv.height / 2) - (this.size[1] / 2) : this.pos[1];
-		if(this.animation){
-			this.anim = new fzn.Animation(this,this.animation);
+		var w,h;
+		if(this.pos == "center"){
+			this.pos = [];
+			this.pos[0] = (this.game.cnv.width / 2) - (this.size[0] / 2);
+			this.pos[1] = (this.game.cnv.height / 2) - (this.size[1] / 2);
+		}else{
+			w = this.parent.size[0] || this.game.cnv.width;
+			h = this.parent.size[1] || this.game.cnv.height;
+			this.pos[0] = (this.pos[0] == "center") ? (w / 2) - (this.size[0] / 2) : this.pos[0];
+			this.pos[1] = (this.pos[1] == "center") ? (h / 2) - (this.size[1] / 2) : this.pos[1];
 		}
+		this.anim = new fzn.Animation(this,this.animation);
 		this.game.loadImage(this.source);
 	},
 	go: function(){
-		if(this.animation){
-			this.anim.go();
-		}
+		this.anim.go();
 		this.redraw();
 	},
 	redraw: function(){
@@ -45,37 +50,48 @@ fzn.Background.prototype = {
 			sX = pos[0];
 			sY = pos[0];
 		}else if(this.fixed === false){
-			y = pos[1] - this.game.level.pos[1];
-			x = pos[0] - this.game.level.pos[0]; 
-			sX = this.game.level.pos[0];
-			sY = this.game.level.pos[1];
+			y = pos[1] - this.parent.pos[1];
+			x = pos[0] - this.parent.pos[0]; 
+			sX = this.parent.pos[0];
+			sY = this.parent.pos[1];
 		}else{
-			x = pos[0] - (this.game.level.pos[0]*this.fixed); 
-			y = pos[1] - (this.game.level.pos[1]*this.fixed);
-			sX = this.game.level.pos[0]*this.fixed;
-			sY = this.game.level.pos[1]*this.fixed;
+			x = pos[0] - (this.parent.pos[0]*this.fixed); 
+			y = pos[1] - (this.parent.pos[1]*this.fixed);
+			sX = this.parent.pos[0]*this.fixed;
+			sY = this.parent.pos[1]*this.fixed;
 		}
-		if(this.opacity != 1){
-			this.game.canvas.globalAlpha = this.opacity;
-		}
-		if(this.repeat == "repeat" || this.repeat == "repeat-x" || this.repeat == "repeat-y"){
-			this.game.canvas.translate(x,y);
-			var ptrn = this.game.canvas.createPattern(this.game.images[this.source],this.repeat);
-			this.game.canvas.fillStyle = ptrn;
+		this.game.canvas.globalAlpha = (this.menu) ?  this.menu.opacity * this.opacity : this.opacity;
+		if(this.color != "transparent"){
+			this.game.canvas.fillStyle = this.color;
 			this.game.canvas.fillRect(
-				sX,
-				sY,
-				this.game.cnv.width,
-				this.game.cnv.height
-			);
-		}else{
-			this.game.canvas.drawImage(
-				this.game.images[this.source],
 				x,
 				y,
 				this.size[0],
 				this.size[1]
 			);
+		}
+		if(this.source){
+			if(this.repeat == "repeat" || this.repeat == "repeat-x" || this.repeat == "repeat-y"){
+				this.game.canvas.translate(x,y);
+				sY = (!this.fixed && (this.repeat == "repeat-x" || this.repeat == "repeat")) ? 0 : sY;
+				sX = (!this.fixed && (this.repeat == "repeat-y" || this.repeat == "repeat")) ? 0 : sX;
+				var ptrn = this.game.canvas.createPattern(this.game.images[this.source],this.repeat);
+				this.game.canvas.fillStyle = ptrn;
+				this.game.canvas.fillRect(
+					sX,
+					sY,
+					this.game.cnv.width,
+					this.game.cnv.height
+				);
+			}else{
+				this.game.canvas.drawImage(
+					this.game.images[this.source],
+					x,
+					y,
+					this.size[0],
+					this.size[1]
+				);
+			}
 		}
 		this.game.canvas.restore();
 	}
